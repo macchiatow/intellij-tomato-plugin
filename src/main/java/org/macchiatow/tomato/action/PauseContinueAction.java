2c777d8ece6ec08b5b3a6b1731369c6de3251174
Togrul Mageramov <cmeisters@gmail.com>
2015-03-26 00:37:25 +0200

    apache licence

--- src/main/java/org/macchiatow/tomato/action/PauseContinueAction.java
+++ src/main/java/org/macchiatow/tomato/action/PauseContinueAction.java
@@ -1,19 +1,28 @@
+/*
+ * Copyright 2015 Togrul Mageramov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.macchiatow.tomato.action;
 
 import com.google.common.base.Function;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.wm.WindowManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.macchiatow.tomato.Initialization;
-
-import javax.swing.*;
 
 /**
- * Created by Togrul Mageramov on 23.03.2015.
+ * Created by Togrul Mageramov on 3/23/2015.
  */
 public class PauseContinueAction extends DefaultAction {
     Presentation presentation;
